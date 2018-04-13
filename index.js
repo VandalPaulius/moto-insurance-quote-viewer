4dc0670bdd040f5ced89b47730f3164eae7cb929
Vandal Paulius Juska <aki.maxis@gmail.com>
2018-04-13 22:40:39 +0100

    Added server CORS allow all

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ const initHttpListener = (database) => {
         console.log('Cannot run without database');
         return;
     }
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    })
+
     app.use(bodyParser.urlencoded({
         extended: false
     }));
